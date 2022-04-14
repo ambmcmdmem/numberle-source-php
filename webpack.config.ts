91bf263c9329819f66e0b05b20adcfb1440a204c
yamamoto <ambmcmdmem@au.com>
2022-04-15 02:11:39 +0900

    vue, Typescript, webpackのセットアップ
    ファイルの構成立て

---
+++ webpack.config.ts
@@ -0,0 +1,27 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
