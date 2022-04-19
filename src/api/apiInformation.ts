2147de240789594559e74b9b13fc14b170df23b0
yamamoto <ambmcmdmem@au.com>
2022-04-19 11:59:19 +0900

    apiのURLとポートをHerokuのものへ

--- src/api/apiInformation.ts
+++ src/api/apiInformation.ts
@@ -1,3 +1,4 @@
-// export const apiUrl = 'https://training-numberle.herokuapp.com';
-export const apiUrl = 'http://localhost';
-export const apiPort = 8000;
+export const apiUrl = 'https://training-numberle.herokuapp.com';
+// export const apiUrl = 'http://localhost';
+// export const apiPort = 8000;
+export const apiPort = process.env.PORT || 5000;
