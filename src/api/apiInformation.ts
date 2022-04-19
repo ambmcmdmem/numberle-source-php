2b1ef97f13cb75889f5f4c72f45c94bda224d3c8
yamamoto <ambmcmdmem@au.com>
2022-04-19 11:53:03 +0900

    ts-nodeを--save-devではなく普通のインストールに

--- src/api/apiInformation.ts
+++ src/api/apiInformation.ts
@@ -1,2 +1,3 @@
-export const apiUrl = 'https://training-numberle.herokuapp.com';
+// export const apiUrl = 'https://training-numberle.herokuapp.com';
+export const apiUrl = 'http://localhost';
 export const apiPort = 8000;
