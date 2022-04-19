64665c165eadec5e05f919ae3d8a7fd04454529e
yamamoto <ambmcmdmem@au.com>
2022-04-19 10:48:27 +0900

    apiのURL先変更

--- src/components/solution/ProposedSolutions.vue
+++ src/components/solution/ProposedSolutions.vue
@@ -22,7 +22,7 @@ import {
 import ProposedSolution from './ProposedSolution.vue';
 import axios from 'axios';
 import { emitter } from '../../modules/emitter';
-import { apiPort } from '../../api/apiInformation';
+import { apiUrl, apiPort } from '../../api/apiInformation';
 
 export default defineComponent({
   components: {
@@ -83,7 +83,7 @@ export default defineComponent({
         ) {
           axios
             .post(
-              `http://localhost:${apiPort}/collation`,
+              `${apiUrl}:${apiPort}/collation`,
               new URLSearchParams({
                 ...parametersAboutSeed.value,
                 ...{ proposedSolution: nowProposedSolution.value.join('') },
@@ -98,7 +98,7 @@ export default defineComponent({
               ) {
                 axios
                   .post(
-                    `http://localhost:${apiPort}/getAnswer`,
+                    `${apiUrl}:${apiPort}/getAnswer`,
                     new URLSearchParams(parametersAboutSeed.value)
                   )
                   .then((response): void =>
