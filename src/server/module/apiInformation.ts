114778fff342a724049bc96979c192cc9322d4ff
yamamoto <ambmcmdmem@au.com>
2022-04-22 04:57:16 +0900

    Heroku用テスト（collationアクセス時,）

--- src/server/module/apiInformation.ts
+++ src/server/module/apiInformation.ts
@@ -1,3 +1,2 @@
 export const apiOrigin = `https://training-numberle.herokuapp.com`;
-// export const accessAllowOrigin = 'https://ambmcmdmem.github.io/numberle';
-export const accessAllowOrigin = 'http://localhost:3000';
+export const accessAllowOrigin = 'https://ambmcmdmem.github.io/numberle';
