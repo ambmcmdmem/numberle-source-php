0800701822e1fc3684ed13b7088f67947d194b03
yamamoto <ambmcmdmem@au.com>
2022-04-22 05:46:37 +0900

    APIのURLを修正

--- src/server/module/apiInformation.ts
+++ src/server/module/apiInformation.ts
@@ -1,2 +1,2 @@
-export const apiOrigin = `https://training-numberle.herokuapp.com`;
+export const apiOrigin = `https://numberle-training.herokuapp.com`;
 export const accessAllowOrigin = 'https://ambmcmdmem.github.io/numberle';
